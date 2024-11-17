d4ceed807bce655f580fe839f1ff0b07645ac846
Aditi jaiswal <aditi.jaiswal@leptonmaps.com>
2024-11-17 17:51:07 +0530

    asfckjndas

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typescript: {
+    ignoreBuildErrors: true, // Ignores TypeScript errors during the build process
+  },
+  reactStrictMode: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
